cab8aa0c972c53291da29fbf7cf348a6e312322d
iuliya21 <iuliya21@mail.ru>
2024-01-16 21:11:56 +0300

    first attemp

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -8,6 +8,7 @@ describe('PostsService', () => {
   });
 
   describe('.findMany', () => {
+    
     const posts = [
       {text: 'Post 1'},
       {text: 'Post 2'},
@@ -20,13 +21,37 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const result = postsService.findMany();
+      expect(result).toEqual(posts);
+      expect(result).toHaveLength(posts.length);
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const skip = 1;
+      const limit = 2;
+      const expectedPosts = posts.slice(skip, skip + limit);
+      const result = postsService.findMany({ skip, limit });
+
+      expect(result).toHaveLength(expectedPosts.length);
+      expect(result).toEqual(expectedPosts);
     });
 
-    // реализуйте недостающие тест-кейсы
+    it('should return correct posts when skip parameter is used', () => {
+      const skip = 2;
+      const expectedPosts = posts.slice(skip);
+      const result = postsService.findMany({ skip });
+
+      expect(result).toHaveLength(expectedPosts.length);
+      expect(result).toEqual(expectedPosts);
+    });
+
+    it('should return correct posts when limit parameter is used', () => {
+      const limit = 2;
+      const expectedPosts = posts.slice(0, limit);
+      const result = postsService.findMany({ limit });
+
+      expect(result).toHaveLength(expectedPosts.length);
+      expect(result).toEqual(expectedPosts);
+    });
   });
 });
